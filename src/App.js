66a9660bb7c72243a40747cb32ea7fc64a2bb1d7
Amy Cox <amy_cox@me.com>
2022-01-17 08:22:47 +0000

    removed monolith component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-// import Monolith from "./components/Monolith";
 import Feature from "./components/Feature";
 
 function App() {
   return (
     <div className="App">
-      {/* <Monolith /> */}
       <Feature />
     </div>
   );
