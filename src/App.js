e2332f053d6f024dd3216b3dba1447790ae1f404
Amy Cox <amy_cox@me.com>
2022-01-17 08:08:43 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Monolith from "./components/Monolith";
+import Feature from "./components/Feature";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Monolith /> */}
+      <Feature />
+    </div>
+  );
+}
+
+export default App;
